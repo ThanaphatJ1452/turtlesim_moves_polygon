a14cb59c68b629bc996d939c83b04403a0bd47fc
thanaphat11017 <thanaphat11017@gmail.com>
2020-08-18 16:33:04 +0700

    add PID control

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Turtlesim moves polygon shape in ROS(Python)
+# Turtlesim moves polygon shape in ROS using PID control(Python)
 ## Result 
-![2020-08-13-23-02-09](https://user-images.githubusercontent.com/53165482/90164403-8bacb680-ddc1-11ea-8e2a-a05b6f47439b.gif)
+![2020-08-18-16-22-53](https://user-images.githubusercontent.com/53165482/90496470-0f72f400-e170-11ea-9143-768e6a8b9b06.gif)
--- turtlesim_movePolygon.py
+++ turtlesim_movePolygon.py
@@ -4,6 +4,7 @@ import rospy
 from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
 import numpy as np
+import time
 
 is_init_pose = False
 is_init_degree = False
@@ -19,9 +20,47 @@ squareDist = 2.0
 
 status = ""
 
+# PID variable
+error = 0
+sumError = 0
+rateError = 0
+currentTime = time.time()
+previousTime = time.time()
+lastError = 0
+
 def poseReceived(position_data): #callback
     global currentPose  
     currentPose = [position_data.x, position_data.y, position_data.theta]
+
+def pidControl(p, i, d, target):
+    global status, currentTime, previousTime, error, sumError, rateError, lastError
+    
+    currentTime = time.time()
+    elapsedTime = (currentTime - previousTime)
+
+    if turnCCW == False:
+        dist = np.sqrt(np.power((currentPose[0] - initPose[0]), 2) + np.power((currentPose[1] - initPose[1]), 2))
+        error = target - dist
+    else:
+        if(currentPose[2] < 0):
+            currentPose[2] = (np.pi*2) - np.abs(currentPose[2])
+        if(initPose[2] < 0):
+            initPose[2] = (np.pi*2) - np.abs(initPose[2])
+        angleDiff = np.abs((currentPose[2]) - (initPose[2]))
+        error = target - angleDiff
+
+    sumError = sumError + (error * elapsedTime)
+    rateError = (error - lastError)/elapsedTime
+
+    try:
+        vel_pid = (p * error) + (i * sumError) + (d * rateError)
+    except:
+        vel_pid = (p * error) + (i * sumError)
+
+    previousTime = currentTime
+    lastError = error
+
+    return vel_pid    
 
 def moveSquare(polygon):
     
@@ -29,12 +68,12 @@ def moveSquare(polygon):
     angleRadius = (angleDegree * 2 * np.pi)/360 
 
     velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
-    pose_subscriber = rospy.Subscriber('/turtle1/pose', Pose, poseReceived)
+    pose_subscriber = rospy.Subscriber('/turtle1/pose', Pose, poseReceived) #subscribe turtlesim data
 
     turtleVel = Twist()
 
     rospy.init_node('moveSquare4turtleSim', anonymous=False)
-    rate = rospy.Rate(20) # 20hz
+    rate = rospy.Rate(1000) # 10hz
 
     while not rospy.is_shutdown():
         global initPose, turnCCW, is_init_pose, status
@@ -50,7 +89,7 @@ def moveSquare(polygon):
                     turnCCW = True
                     is_init_pose = False
                 else:
-                    turtleVel.linear.x = turtleVelX
+                    turtleVel.linear.x = pidControl(4, 0, 0, squareDist)
                     status = "Going Forward: " + str(dist)
         else:
             if(currentPose[2] < 0):
@@ -65,7 +104,7 @@ def moveSquare(polygon):
                 turtleVel.angular.z = 0
                 turnCCW = False
             else:
-                turtleVel.angular.z = turtleAngZ
+                turtleVel.angular.z = pidControl(4, 0, 0, angleRadius)
 
         velocity_publisher.publish(turtleVel)
 
